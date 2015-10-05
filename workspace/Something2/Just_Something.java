52d5f3e243cb0585f67a1a0c78a66c98e5703892
D22 <judenelson468@gmail.com>
2015-10-05 10:35:33 -0500

    Just_Something.java

---
+++ workspace/Something2/Just_Something.java
@@ -0,0 +1,54 @@
+
+public class Just_Something {
+	 public static void main(String[] args) {
+	        int i = 1;
+	        float a = 2.5f;
+	        double b = 3.87;
+	        short c = 16;
+	        char d1 = ';';
+	        char d2 = 'a';
+	        char d3 = 42;
+	        char d4 = 145;
+	        String e = d4+"";
+	        String e1 = "Amanda is the best";
+	        boolean f = true;
+	        boolean g = i % 2 == 0;
+	        /*
+	        here we will look at some mathematical operators
+	        */
+	        // + this adds two numbers and more
+	        // - this subtracts two numbers and more
+	        // * this multiplies two numbers and more
+	        // / this divides two numbers and more
+	        // % this is modulo. Shows you the remainder
+	        System.out.println(i + " an integer");
+	        System.out.println(a + " a float");
+	        System.out.println(b + " a double");
+	        System.out.println(c + " a short");
+	        System.out.println(d1 + " a char");
+	        System.out.println(d2 + " a char");
+	        System.out.println(d3 + " a char");
+	        System.out.println(e + " a string");
+	        System.out.println(f + " a boolean");
+	        for(i = -123;i < 123; i++) {   
+	            //System.out.println((char) i);
+	        }
+	            System.out.println(i % 2 == 0);
+	      
+	    /* % checks for the remainder
+	        i % 2 checks if a number is even or odd
+	    */
+	    i = 0;
+	    a = 0;
+	    e = "ninja";
+	    System.out.println(i + a + " an integer " + "plus a float");
+	    if (i % 2 == 0) {
+	    	System.out.println(i + " is not a prime number");
+	    } else {
+	    	System.out.println(i + " is a prime number");
+	    		
+	    	}
+	    }
+	 }
+
+		
